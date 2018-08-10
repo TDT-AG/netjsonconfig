a7a8b9c921b4d555d3289f3edf7ecb71b68adb83
Oliver Kraitschy <ok@dev.tdt.de>
2018-08-10 14:34:07 +0200

    Extend schema and work on converter, fix tests

--- netjsonconfig/backends/openwrt/converters/firewall.py
+++ netjsonconfig/backends/openwrt/converters/firewall.py
@@ -7,34 +7,75 @@ from .base import OpenWrtConverter
 class Firewall(OpenWrtConverter):
     netjson_key = 'firewall'
     intermediate_key = 'firewall'
-    _uci_types = ['defaults', 'forwarding', 'zone']
+    _uci_types = ['defaults', 'forwarding', 'zone', 'rule']
     _schema = schema['properties']['firewall']
 
     def to_intermediate_loop(self, block, result, index=None):
-        if block:
-            provider_list = self.__intermediate_providers(block.pop('providers', {}))
-            block.update({
-                '.type': 'defaults',
-                '.name': block.pop('id', None),
-            })
-            result.setdefault('firewall', [])
-            result['firewall'] = [self.sorted_dict(block)] + provider_list
+        forwardings = self.__intermediate_forwardings(block.pop('forwardings', {}))
+        zones = self.__intermediate_zones(block.pop('zones', {}))
+        rules = self.__intermediate_rules(block.pop('rules', {}))
+        block.update({
+            '.type': 'defaults',
+            '.name': block.pop('id', 'defaults'),
+        })
+        result.setdefault('firewall', [])
+        result['firewall'] = [self.sorted_dict(block)] + forwardings + zones + rules
         return result
 
-    def __intermediate_providers(self, providers):
+    def __intermediate_forwardings(self, forwardings):
         """
-        converts NetJSON provider to
+        converts NetJSON forwarding to
         UCI intermediate data structure
         """
         result = []
-        for provider in providers:
-            uci_name = self._get_uci_name(provider['lookup_host'])
-            resultdict = OrderedDict((('.name', uci_name),
-                                      ('.type', 'service')))
-            for key, value in provider.items():
-                resultdict[key] = value
+        for forwarding in forwardings:
+            resultdict = OrderedDict((('.name', self.__get_auto_name_forwarding(forwarding)),
+                                      ('.type', 'forwarding')))
+            resultdict.update(forwarding)
             result.append(resultdict)
         return result
+
+    def __get_auto_name_forwarding(self, forwarding):
+        if 'family' in forwarding.keys():
+            uci_name = self._get_uci_name('_'.join([forwarding['src'], forwarding['dest'],
+                                          forwarding['family']]))
+        else:
+            uci_name = self._get_uci_name('_'.join([forwarding['src'], forwarding['dest']]))
+        return 'forwarding_{0}'.format(uci_name)
+
+    def __intermediate_zones(self, zones):
+        """
+        converts NetJSON zone to
+        UCI intermediate data structure
+        """
+        result = []
+        for zone in zones:
+            resultdict = OrderedDict((('.name', self.__get_auto_name_zone(zone)),
+                                      ('.type', 'zone')))
+            resultdict.update(zone)
+            result.append(resultdict)
+        return result
+
+    def __get_auto_name_zone(self, zone):
+        return 'zone_{0}'.format(self._get_uci_name(zone['name']))
+
+    def __intermediate_rules(self, rules):
+        """
+        converts NetJSON rule to
+        UCI intermediate data structure
+        """
+        result = []
+        for rule in rules:
+            if 'config_name' in rule:
+                del rule['config_name']
+            resultdict = OrderedDict((('.name', self.__get_auto_name_rule(rule)),
+                                      ('.type', 'rule')))
+            resultdict.update(rule)
+            result.append(resultdict)
+        return result
+
+    def __get_auto_name_rule(self, rule):
+        return 'rule_{0}'.format(self._get_uci_name(rule['name']))
 
     def to_netjson_loop(self, block, result, index):
         result['firewall'] = self.__netjson_firewall(block)
